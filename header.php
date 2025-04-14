eb04757e01a366635d8c4c7974a516df355ce6dd
Bishal Shrestha <bishal.shrestha@outlook.com.au>
2025-04-14 20:25:16 +1000

    - reorganised the header

--- header.php
+++ header.php
@@ -1,15 +1,8 @@
 <!DOCTYPE html>
 <html>
-<head>
-		
-	<a href="../index.php"><img src="../images/logo.jpg" alt="airtra" style="width:150px;height:30px;">AIRTRA: SEARCH | TRAVEL | EXPLORE
-
-	</a>
-    <title>AIRTRA: SEARCH | TRAVEL | EXPLORE</title>
-
-</head>
-<body>
-     
-</body>
+	<head>
+		<a href="index.php"><img src="assets/img/logo.jpg" alt="airtra" style="width:150px;height:30px;">AIRTRA: SEARCH | TRAVEL | EXPLORE</a>
+		<title>AIRTRA: SEARCH | TRAVEL | EXPLORE</title>
+	</head>
 </html>
 <br>
