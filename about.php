63932c6040993ac4f7c576d665e9d04935896dbb
Bishal Shrestha <bishal.shrestha@outlook.com.au>
2025-04-14 21:31:03 +1000

    about page

--- about.php
+++ about.php
@@ -1,42 +1,48 @@
-
-<?php
-//include 'core/init.php';
-//include 'includes/overall/header.php';?>
-
-
-<h2 style="font-size: 30px"> <h2 style="color:white"> About us </h2></h2>
-	<p style="font-size: 20px"> <p style="color: white">
-AirTra mission is to become Nepalese preferred low-cost airline,delivering the lowest air fares
-with the highest consumer value,to price sensitive consumers to fulfill everyones dream of flying!
-With Nepal's economic and business growth,the percentage of traveling population is burgeoning. Nepalese are traveling for both business,pleasure and everyone needs to save both time and money.
-AirTra vision is to address that and ensure that flying is for everyone.
-The power to fly for everyone with a dynamic fare structure. <br>
-	</p> </p>
-	<h3 style="font-size: 30px"> <h3 style="color:white"> Careers </h3></h3>
-	<p style="font-size: 20px"> <p style="color: white">
-	At AirTra, we believe that our people are our greatest asset. We are always on a lookout for highly
-	motivated individuals who can collaborate with like-minded people in an environment that embraces
-	individuality and rewards your best work. A career opportunity at AirTra will entail you to unlimited 
-	opportunities and a host of benefits. Come, be a part of AirTra.<br>
-	</p></p>
-		<h3 style="font-size: 30px"> <h3 style="color:white">Developers</h3></h3>
-	<p style="font-size: 20px"> <p style="color: white">
-	Bishal Shrestha (014-321)<br> Hari Thapa (014-320) <br> Biraj Paudel (014-319) <br>
+<!DOCTYPE html>
+<html>
+<h2 style="font-size: 30px"> 
+	<h2 style="color:white"> About us </h2>
+</h2>
+<p style="font-size: 20px"> 
+	<p style="color: white">
+		AirTra mission is to become Nepalese preferred low-cost airline,delivering the lowest air fares
+		with the highest consumer value,to price sensitive consumers to fulfill everyones dream of flying!
+		With Nepal's economic and business growth,the percentage of traveling population is burgeoning. Nepalese are traveling for both business,pleasure and everyone needs to save both time and money.
+		AirTra vision is to address that and ensure that flying is for everyone.
+		The power to fly for everyone with a dynamic fare structure. 
+		<br>
+	</p> 
+</p>
+<h3 style="font-size: 30px"> 
+	<h3 style="color:white"> Careers </h3>
+</h3>
+<p style="font-size: 20px"> 
+	<p style="color: white">
+		At AirTra, we believe that our people are our greatest asset. We are always on a lookout for highly
+		motivated individuals who can collaborate with like-minded people in an environment that embraces
+		individuality and rewards your best work. A career opportunity at AirTra will entail you to unlimited 
+		opportunities and a host of benefits. Come, be a part of AirTra.
+		<br>
 	</p>
-	<div>
+</p>
+<h3 style="font-size: 30px"> 
+	<h3 style="color:white">Developer</h3>
+</h3>
+<p style="font-size: 20px"> 
+	<p style="color: white">
+		Bishal Shrestha <br>
+	</p>
+</p>
+<div>
 	<br>
 	<br>
-	<style>
+</div>
+<?php echo file_get_contents( "copyright.php" ); ?>
+</html>
+<style>
     body{
-    background-image: url("../images/AirTra.jpg");
+    background-image: url("assets/img/AirTra.jpg");
     background-size: 1400px 900px;
     background-repeat: no-repeat;
  }
-    </style> 
-			<!--		<img src="../images/AirTra.jpg"> -->
-					</div>
-<?php // include 'includes/overall/footer.php';?>
-<?php
-echo file_get_contents( "copyright.php" ); 
-?>
-</font>
+ </style> 
