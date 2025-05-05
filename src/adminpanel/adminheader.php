1d9e9bc7cb6788bd81ba84c9cf4fd93d3b3f3228
Bishal Shrestha <bishal.shrestha@outlook.com.au>
2025-05-05 11:52:22 +1000

    readded adminheader

---
+++ src/adminpanel/adminheader.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+		
+	<a href="adminindex.php"><img src="../public/assets/img/logo.jpg" alt="airtra" style="width:150px;height:30px;">AIRTRA: SEARCH | TRAVEL | EXPLORE
+
+	</a>
+    <title>AIRTRA: SEARCH | TRAVEL | EXPLORE</title>
+
+</head>
+<body>
+     
+</body>
+</html>
+<br>
